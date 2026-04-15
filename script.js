eb44820e9652fd2ea23a7a16cbfcfa03bce494e5
Haseeb <ah085904@gmail.com>
2026-04-15 15:48:13 +0500

    reverting changes

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 const gridContainer = document.querySelector(".grid-container");
-
-for (let i = 1; i <= 32; i++) {
-  let newBox = document.createElement("div");
-  newBox.textContent = `${i}`;
-  newBox.classList.add("grid-box");
-  gridContainer.appendChild(newBox);
-}
