d3e8472da7fce1ecc3dcbbe3e80f87b44f59d823
Haseeb <ah085904@gmail.com>
2026-04-15 16:15:41 +0500

    trying to make the grid work

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 const gridContainer = document.querySelector(".grid-container");
+
+for (let i = 1; i <= 16; i++) {
+  const gridRow = document.createElement("div");
+  gridRow.classList.add("grid-row");
+  let newBox = document.createElement("div");
+  newBox.classList.add("grid-box");
+  newBox.textContent = `${i}`;
+  gridRow.appendChild(newBox);
+  for (let j = 1; j <= 16; j++) {
+    gridContainer.appendChild(gridRow);
+  }
+}
