c4134e6c59a432873550a98325d76e1951d79791
Haseeb <ah085904@gmail.com>
2026-04-15 19:24:59 +0500

    removed numbering from box from js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ for (let i = 1; i <= 16; i++) {
   for (let j = 1; j <= 16; j++) {
     let box = document.createElement("div");
     box.classList.add("grid-box");
-    box.textContent = `${j}`;
+
     row.appendChild(box);
   }
   gridContainer.appendChild(row);
