c008510e8ebac9f1072c951cbb8dbfe7d44871a9
Haseeb <ah085904@gmail.com>
2026-04-15 20:36:35 +0500

    increaased max of grid-container

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const gridContainer = document.querySelector(".grid-container");
 
-for (let i = 1; i <= 16; i++) {
+for (let i = 1; i <= 64; i++) {
   let row = document.createElement("div");
   row.classList.add("row");
 
-  for (let j = 1; j <= 16; j++) {
+  for (let j = 1; j <= 64; j++) {
     let box = document.createElement("div");
     box.classList.add("grid-box");
 
